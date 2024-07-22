06f518e60967eb77fe1f735952c039edd290b1fa
Sheftyaldi <130757857+Sheftyaldi@users.noreply.github.com>
2024-07-23 01:44:44 +0800

    Membuat Project Input Form Sederhana

---
+++ Belajar_programming/views/input_data.php
@@ -0,0 +1,18 @@
+<div class="container mt-3">
+  <h2>Input Data</h2>
+  <form action="/controller/proses_input_data.php" method="post">
+    <div class="mb-3 mt-3">
+      <label>Nama:</label>
+      <input type="text" class="form-control" placeholder="Ketik Nama" name="nama">
+    </div>
+    <div class="mb-3 mt-3">
+      <label>Nim:</label>
+      <input type="text" class="form-control" placeholder="Ketik Nim" name="nim">
+    </div>
+    <div class="mb-3 mt-3">
+      <label>Jurusan</label>
+      <input type="text" class="form-control" placeholder="Ketik Jurusan" name="jurusan">
+    </div>
+    <button type="submit" class="btn btn-primary">Input Data</button>
+  </form>
+</div>
